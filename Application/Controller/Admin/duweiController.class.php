6a3b6b090656d9962fd993dbe9c8a8eb0e095f69
renzhiying <171271200@qq.com>
2018-10-25 17:54:04 +0800

    对控制器的修改

--- Application/Controller/Admin/duweiController.class.php
+++ Application/Controller/Admin/duweiController.class.php
@@ -5,9 +5,4 @@ class duweiController {
     public function duAction(){
         echo "杜威有毒";
     }
-    
-    //新增测试方法
-    public function testAction(){
-        echo "测试Git的拉取功能，也就是提交之前的更新，将Github上有过更新的代码拉取到本地的代码仓库";
-    }
 }
